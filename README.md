80163ba77608f65fc8c9535cc1fc452b9902190f
neonbl4 <sasha-neo@list.ru>
2021-01-15 09:32:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jqjuhv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jqjuhv)
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_url: '', //life
+  DEBUG: false,// if false we will not have console.log infos in development build
+  encrypt_token: ''
+};
